48a2dead9dbdf592cc763540b5341a1ec7c2d554
Thomas Streefkerk <thomas.streefkerk@code-orange.nl>
2022-11-30 15:08:50 +0100

    add Player filter method

--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -1,7 +1,13 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Player} from "../assets/interfaces/player";
-import {map, Observable, ReplaySubject} from "rxjs";
+import {BehaviorSubject, combineLatest, first, map, Observable, ReplaySubject} from "rxjs";
+
+
+interface Filter {
+	key: string,
+	value: number | string | null
+}
 
 @Injectable({
 	providedIn: 'root'
@@ -9,6 +15,8 @@ import {map, Observable, ReplaySubject} from "rxjs";
 export class PlayerService {
 
 	players$: ReplaySubject<Player[]> = new ReplaySubject<Player[]>();
+	filteredPlayers$: ReplaySubject<Player[]> = new ReplaySubject<Player[]>();
+	filters$: BehaviorSubject<Filter[]> = new BehaviorSubject<Filter[]>([]);
 
 	constructor(private http: HttpClient) {
 		this.http.get('assets/data/player_data.csv', {responseType: 'text'}).subscribe(
@@ -16,18 +24,38 @@ export class PlayerService {
 				const rows = data.split('\n');
 				const headers = rows[0].split(';');
 
-				this.players$.next(
-					rows.slice(1).map(row => {
-						const arr = row.split(';');
-						const player = {};
-						for (let i = 0; i < arr.length - 1; i++) {
-							player[headers[i]] = arr[i];
-						}
-						return player as Player;
-					})
-				);
+				const players = rows.slice(1).map(row => {
+					const arr = row.split(';');
+					const player = {};
+					for (let i = 0; i < arr.length - 1; i++) {
+						player[headers[i]] = arr[i];
+					}
+					return player as Player;
+				});
+				this.players$.next(players);
+				this.filteredPlayers$.next(players);
 			}
 		);
+
+		combineLatest(this.players$, this.filters$).subscribe(([players, filters]) => {
+			this.filteredPlayers$.next(
+				players.filter(player => {
+					for (let i = 0; i < filters.length; i++) {
+						const filter = filters[i];
+						if (typeof filter.value === 'number') {
+							if (player[filter.key] <= filter.value) {
+								return false;
+							}
+						} else {
+							if (player[filter.key] !== filter.value) {
+								return false;
+							}
+						}
+					}
+					return true;
+				})
+			);
+		});
 	}
 
 	getTopRatedPlayers$(n: number, reverse = false): Observable<Player[]> {
@@ -39,5 +67,24 @@ export class PlayerService {
 				return b.overall - a.overall;
 			}).slice(0, n))
 		);
+	}
+
+	// Pass a new filter, update an existing one, or pass value: null to remove filter.
+	updateFilters(filter: Filter) {
+		this.filters$.pipe(first()).subscribe(filters => {
+			const index = filters.findIndex(obj => obj.key === filter.key);
+			if (index === -1) {
+				// Add new filter
+				this.filters$.next(filters.concat(filter));
+			} else if (filter.value === null) {
+				// Remove existing filter
+				filters.splice(index, 1);
+				this.filters$.next(filters);
+			} else {
+				// Update existing filter
+				filters[index].value = filter.value;
+				this.filters$.next(filters);
+			}
+		})
 	}
 }
