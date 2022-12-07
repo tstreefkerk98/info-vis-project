29cf6b6961c5d6675ce40e4ddcc1eb79378a524e
Whebon <bart-man99@hotmail.com>
2022-12-07 15:35:17 +0100

    PCP plot split into 'selected' and 'filtered'

--- src/app/pcp-plot/pcp-plot.component.ts
+++ src/app/pcp-plot/pcp-plot.component.ts
@@ -17,8 +17,16 @@ export class PcpPlotComponent implements OnInit {
 
 	radius: number = 250;
 	features: string[] = ['pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic'];
-	allPCPLines: any = null
-	selectedPCPLines: any = null;
+
+	svg = d3.select("#my_dataviz");
+	axis: any;
+	lines = {
+		selected: null,
+		all: null
+	}
+	xScale: any = null;
+	yScale: any = null;
+
 
 	constructor(public playerService: PlayerService) {
 	}
@@ -32,83 +40,98 @@ export class PcpPlotComponent implements OnInit {
 			height = 400 - margin.top - margin.bottom;
 
 		// append the svg object to the body of the page
-		const svg = d3.select("#my_dataviz")
+		this.svg = d3.select("#my_dataviz")
 			.append("svg")
 			.attr("width", width + margin.left + margin.right)
 			.attr("height", height + margin.top + margin.bottom)
 			.append("g")
-			.attr("transform",
-				`translate(${margin.left},${margin.top})`);
+			.attr("transform", `translate(${margin.left},${margin.top})`);
 
-
-		//remove deselected players
-		// this.playerService.lastSelectedPlayer$.subscribe(player => {
-		// 	if (player.color === null) {
-		// 		const index = this.pathAssignments.findIndex(assignment => assignment.playerId === player.player.sofifa_id);
-		// 		this.pathAssignments[index].path.remove();
-		// 		return;
-		// 	}
-
-		this.playerService.players$.subscribe(allPlayers => {
-
-		})
-
-		// draw selected players on top of the graph
-		this.playerService.selectedPlayers$.subscribe(selectedPlayers => {
+		this.playerService.filteredPlayers$.subscribe(allPlayers => {
 			// For each dimension, I build a linear scale. I store all in a y object
-			const players = selectedPlayers.map(selectedPlayers => selectedPlayers.player)
-			const y = {}
+			this.yScale = {}
 			for (let i in this.features) {
 				let name = this.features[i]
-				y[name] = d3.scaleLinear()
-					.domain(d3.extent(players, function(d) { return +d[name]; }))
+				this.yScale[name] = d3.scaleLinear()
+					.domain(d3.extent(allPlayers, function(d) { return +d[name]; }))
 					.range([height, 0])
 			}
 
 			// Build the X scale -> it find the best position for each Y axis
-			let x = d3.scalePoint()
+			this.xScale = d3.scalePoint()
 				.range([0, width])
 				.padding(1)
 				.domain(this.features);
 
-			// The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
-			function path(obj) {
-				return d => d3.line()(obj.features.map(p => [x(p), y[p](d.player[p])]));
-			}
-
-			//create a new pcp group
-			let group = svg.append('g');
-
-			// Draw the lines
-			group.selectAll("myPath")
-				.data(selectedPlayers)
-				.join("path")
-				.attr("d",  path(this))
-				.style("fill", "none")
-				.style("stroke", selectedPlayers => selectedPlayers.color)
-				.style("opacity", 1)
-
-			// Draw the axis:
-			group.selectAll("myAxis")
-				// For each dimension of the dataset I add a 'g' element:
-				.data(this.features).enter()
-				.append("g")
-				// I translate this element to its right position on the x axis
-				.attr("transform", function(d) { return "translate(" + x(d) + ")"; })
-				// And I build the axis with the call function
-				.each(function(d) { d3.select(this).call(d3.axisLeft(y[d])); })
-				// Add axis title
-				.append("text")
-				.style("text-anchor", "middle")
-				.attr("y", -11)
-				.text(function(d) { return d; })
-				.style("fill", "black")
-
-			// overwrite the previous pcp lines
-			if (this.selectedPCPLines != null){
-				this.selectedPCPLines.remove()
-			}
-			this.selectedPCPLines = group
+			this.drawAxis()
+			this.drawLines("all", allPlayers)
 		})
+
+		// draw selected players on top of the graph
+		this.playerService.selectedPlayers$.subscribe(selectedPlayers => {
+			this.drawLines("selected", selectedPlayers)
+		})
+	}
+
+	drawAxis(){
+		//create new axis
+		const group = this.svg.append('g');
+		const x = this.xScale
+		const y = this.yScale
+		group.selectAll("myAxis")
+			// For each dimension of the dataset I add a 'g' element:
+			.data(this.features).enter()
+			.append("g")
+			// I translate this element to its right position on the x axis
+			.attr("transform", function(d) { return "translate(" + x(d) + ")"; })
+			// And I build the axis with the call function
+			.each(function(d) { d3.select(this).call(d3.axisLeft(y[d])); })
+			// Add axis title
+			.append("text")
+			.style("text-anchor", "middle")
+			.attr("y", -11)
+			.text(function(d) { return d; })
+			.style("fill", "black")
+
+		// overwrite the previous axis
+		if (this.axis != null){
+			this.axis.remove()
+		}
+		this.axis = group
+	}
+
+	drawLines(data_type: string, data: any){
+		let path, color, opacity, stroke_width
+		if (data_type == "selected") {
+			path = d => d3.line()(this.features.map(p => [this.xScale(p), this.yScale[p](d.player[p])]))
+			color = selectedPlayers => selectedPlayers.color
+			opacity = 1
+			stroke_width = 4
+		}
+		else{
+			data_type = "all"
+			path = d => d3.line()(this.features.map(p => [this.xScale(p), this.yScale[p](d[p])]))
+			color = "black"
+			//TODO: dynamically choose the opacity (based on number of players)
+			opacity = 0.2
+			stroke_width = 1
+		}
+
+		// Draw the lines
+		let group = this.svg.append('g');
+		group.selectAll("myPath")
+			.data(data)
+			.join("path")
+			.attr("d",  path)
+			.style("fill", "none")
+			.style("stroke", color)
+			.style("opacity", opacity)
+			.style("stroke-width", stroke_width)
+
+		// Overwrite the previous lines
+		if (this.lines[data_type] != null){
+			this.lines[data_type].remove()
+		}
+		this.lines[data_type] = group
 	}
 }
